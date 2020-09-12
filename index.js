939d4a91f86c06d4418adbb3abdd9b2b78936019
LucasLaibly <original7245@gmail.com>
2020-09-11 21:26:03 -0500

    post message in channel, bot auto react via emote

---
+++ index.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const { env, disconnect } = require('process');
+const bot = new Discord.Client( {partials: ['MESSAGE', "CHANNEL", "REACTION"]} );
+const token = 'NzU0MTM5MDU5MjYxMjc2MTgw.X1wYug.s_pWSIAhAkeJiqJKRKisDy9z6Lc';
+const prefix = '!';
+
+bot.on('ready', () => {
+    console.log('Time For Movie Night.');
+
+    bot.user.setActivity('Movie Night Bot!').catch(console.error);
+});
+
+bot.on('message', async message => {
+
+    if (message.author.bot || message.channel.type === "dm") return;
+
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = message.content.substring(message.content.indexOf(' ')+1);
+
+    if(!message.content.startsWith(prefix)) return;
+
+    if (cmd === `${prefix}reactions`){
+        const embed = new Discord.MessageEmbed()
+                    .setTitle('Reaction Roles')
+                    .setDescription('React to get a role.')
+                    .setColor('GREEN');
+    
+        let msgEmbed = await message.channel.send(embed);
+        msgEmbed.react('😇');
+    }
+})
+
+
+
+bot.login(token);
